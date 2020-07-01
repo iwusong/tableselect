340745579620c30d54826100384752b34636153b
ruytzzz <1570044080@qq.com>
2020-07-01 12:35:35 +0800

    初始化

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,110 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+ <template>
+  <div>
+    <div>
+      <button @click="clickbutton(index)" v-for="(item,index) in tabledata[0]" :key="item">{{item}}</button>
+    </div>
+    <table>
+      <table border="1">
+        <tr v-for="item in tabledata" :key="item.key">
+          <td v-show="!flag[index]" v-for="(data,index) in item" :key="data.key">{{data}}</td>
+        </tr>
+      </table>
+    </table>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      tabledata: [
+        [
+          "ID",
+          "AA",
+          "BB",
+          "CC",
+          "DD",
+          "EE",
+          "FF",
+          "GG",
+          "ZZ",
+          "XX",
+          "CC",
+          "VV"
+        ],
+        [
+          "9527",
+          "AA",
+          "BB",
+          "CC",
+          "DD",
+          "EE",
+          "FF",
+          "GG",
+          "ZZ",
+          "XX",
+          "CC",
+          "VV"
+        ],
+        [
+          "9528",
+          "AA",
+          "BB",
+          "CC",
+          "DD",
+          "EE",
+          "FF",
+          "GG",
+          "ZZ",
+          "XX",
+          "CC",
+          "VV"
+        ],
+        [
+          "9529",
+          "AA",
+          "BB",
+          "CC",
+          "DD",
+          "EE",
+          "FF",
+          "GG",
+          "ZZ",
+          "XX",
+          "CC",
+          "VV"
+        ]
+      ],
+      flag: {}
+    };
+  },
+  created() {
+    this.tabledata[0].forEach((item, index) => {
+      this.flag[index] = false;
+    });
+    console.log(this.flag);
+  },
+  methods: {
+    clickbutton(index) {
+      this.flag[index] = !this.flag[index];
+      this.$forceUpdate();
+    }
   }
-}
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+table {
+  font-size: 0.6vw;
+  color: #686868;
+  border-collapse: collapse;
+}
+td {
+  padding: 0.5vw;
+  border: solid 1px #686868;
+  width: 50px;
+}
+button {
+  width: 61.5px;
 }
 </style>
